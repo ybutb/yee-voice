d589461dfae8433bb9ec05d90d51c362cbcb6e5d
Ivan Ovcharenko <ivan.ovcharenko@sigma.software>
2018-08-16 22:35:43 +0300

    Updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Server to control yeelight bulb devices using Pocket sphinx.
 
 1. Yee bulb MAC and IP should be set in config/config.json
-2. Run `npm install` and `npm run run`
+2. Run `npm run run`
+3. To set up voice recognition see: https://github.com/cmusphinx/pocketsphinx.
+Existing vocabulary could be found at config/pocketphinx/commands.txt
--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const logger = require( './dist/logger' );
 const process = require('process');
 const YeeController = require("./dist/controllers/YeeController.js");
 const express = require('express');
-const debug = require('debug')('http');
-const YeeClient       = require('./dist/components/yee-client/YeeClient');
-
 
 const app = express();
 const PORT = process.env.PORT || 5858;
@@ -26,7 +23,5 @@ process.on('warning', (warning) => {
 
 app.listen(PORT);
 YeeController.init();
-
-// new YeeClient();
 
 process.once('SIGTERM', process.exit);
